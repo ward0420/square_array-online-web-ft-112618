0e3cfc4c8633c3aee43c94d3a3eb1552fcb8fcbb
Don Ward <10is0420@gmail.com>
2018-11-29 01:30:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers = [1,2,3]
+  numbers.each do |number|
+    number ** 2
+  end
 end
+
+#brothers = ["Tim", "Tom", "Jim"]
+#brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
