1f76846249e4afe28aa9152acfcbe9733b3b8691
Don Ward <10is0420@gmail.com>
2018-12-01 15:23:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 def square_array(array)
   numbers = []
   array.each do |number|
     numbers << number ** 2
   end
   return numbers
+=======
+def square_array(numbers)
+  numbers = [1,2,3]
+  numbers.each do |number|
+    number ** 2
+  end
+>>>>>>> 6160576193c656ab2dd97fe7112630e6e417ea59
 end
